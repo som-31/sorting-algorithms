b5f18687a833b08a696bab4fd98c4e5d7f1323e5
som-31 <Somnath3151995@gmail.com>
2021-11-21 13:27:55 -0600

    quick sort

---
+++ quickSort.js
@@ -0,0 +1,21 @@
+console.log('This is quick Sort');
+
+function quickSort(array){
+    if(array.length <= 1){
+        return array;
+    }
+
+    const pivot = array[array.length - 1];
+    const leftArr = [];
+    const rightArr = [];
+
+    for (const el of array.slice(0, array.length - 1)) {
+        el < pivot ? leftArr.push(el) : rightArr.push(el);        
+    }
+    return [...quickSort(leftArr), pivot, ...quickSort(rightArr)];
+}
+
+const unSortedArray = [2,4,5,121,442,1,2,56,6,565,7];
+console.log(quickSort(unSortedArray));
+
+
